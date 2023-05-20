e252ba6ae2990e7a3a85028305e6278965ccf719
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-05-20 23:04:40 +0600

    updete hasd done

--- src/pages/MyToys/UpdateToys/UpdateToys.jsx
+++ src/pages/MyToys/UpdateToys/UpdateToys.jsx
@@ -1,8 +1,10 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const UpdateToys = () => {
     const updateToy = useLoaderData();
+    const { _id } = updateToy;
     console.log(updateToy);
     const handleUpdateToy = (event) => {
         event.preventDefault();
@@ -13,13 +15,35 @@ const UpdateToys = () => {
         const updateToy = { price, quantity }
         console.log(updateToy);
 
+        fetch(`http://localhost:5000/allToys/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateToy)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    Swal.fire(
+                        'The Internet?',
+                        'That thing is still around?',
+                        'question'
+                    )
+                }
+            })
     }
+
+
+
 
     return (
         <form onSubmit={handleUpdateToy}>
 
             <input type="text" placeholder="price" name="price" className="input input-bordered input-primary w-full max-w-xs mr-4" />
             <input type="text" placeholder="quantity" name="quantity" className="input input-bordered input-primary w-full max-w-xs" />
+            <input type="submit" value="Update" />
         </form>
     );
 };
