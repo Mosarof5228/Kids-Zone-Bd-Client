c4e63816d9f83d44b6648fa18cac25448d7a4ed6
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-05-21 22:57:42 +0600

    logo and website name modified

--- src/pages/Category/CategoryViewDetails.jsx
+++ src/pages/Category/CategoryViewDetails.jsx
@@ -3,11 +3,22 @@ import { useLoaderData } from "react-router-dom";
 
 const CategoryViewDetails = () => {
     const details = useLoaderData();
-    const { name } = details;
+    console.log(details)
+    const { name, photo, price, saller_name, saller_email, category, quantity } = details;
     return (
-        <div>
-            <h2>This is category beww diedf</h2>
-            <h3>Name: {name}</h3>
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img className="h-80 w-80 p-4" src={photo} alt="Movie" /></figure>
+            <div className="card-body">
+
+                <h2 className="card-title">Saller Name: {saller_name}</h2>
+                <p>Category: {category}</p>
+                <p>Price: {price}</p>
+                <p>Quantity: {quantity}</p>
+                <p><small>saller_email: {saller_email}</small></p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Watch</button>
+                </div>
+            </div>
         </div>
     );
 };
